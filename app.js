d1f2af9d61581ae384ced431ad13d7d03cc0ba2e
Your Name <you@example.com>
2021-02-28 12:25:29 +0300

    Added function, rename button, added actual N of support tab

--- app.js
+++ app.js
@@ -12,56 +12,51 @@ function onClick() {
   };
 	
 
+var win_html3 = `<div style="display: flex;">
+<button id="btn" type="button" onClick="onClick()"> Нажми на меня </button>
+  <p>Количество обращений в Support tab: <a id="clicks">0</a></p> 
+          </div>`;
 
-  var win_html3 = `<div style="display: flex;">
-  <button id="btn" type="button" onClick="onClick()">Click me</button>
-   <p>Clicks: <a id="clicks">0</a></p> 
-            </div>`;
+let wint3 = document.createElement('div');
+document.body.append(wint3);
+wint3.style = 'min-height: 20px; max-height: 750px; min-width: 35px; max-width: 370px; background: wheat; top:405px ; left:562px;   font-size: 14px; z-index: 20; position: fixed; border: 1px solid rgb(56, 56, 56); color: black';
+wint3.innerHTML = win_html3;
 
-  let wint3 = document.createElement('div');
-    document.body.append(wint3);
-    wint3.style = 'min-height: 20px; max-height: 750px; min-width: 35px; max-width: 370px; background: wheat; top:0px ; left:50px;   font-size: 14px; z-index: 20; position: fixed; border: 1px solid rgb(56, 56, 56); color: black';
-    // wint3.setAttribute('id', 'clicks');
-    wint3.innerHTML = win_html3;
+document.getElementById('btn').onclick = function (){
+  clicks += 1;
+  document.getElementById("clicks").textContent = clicks;
+
+  function myFunc () {
+    supportTabNew = clicks;
+    supportTabOld = supportTabNew;
+
+
+  }
+
+  }
+
+
+let findElement = document.querySelector('[data-fieldtypecompletekey="com.atlassian.jira.plugin.system.customfieldtypes:float"]');
+
+var supportTabOld,supportTabNew;
+
+if(findElement !== null )
+{
+  console.log("Я нашел Support Tab");
+  findElement[1];
+  supportTabOld = document.getElementById("customfield_15410-val").textContent;
+  supportTabOld = parseInt(supportTabOld);
+  console.log("Слышь, тут support tab =", supportTabOld);
   
-    document.getElementById('btn').onclick = function (){
-      clicks += 1;
-      document.getElementById("clicks").textContent = clicks;
-     }
- 
-  
-  //  if(document.getElementById('customfield-panel-3') != undefined){
-  //   console.log("Я тебя нашел");
-  //  }
+  clicks = supportTabOld;
+  document.getElementById("clicks").textContent = clicks;
 
-   let findElement = document.querySelector('[data-fieldtypecompletekey="com.atlassian.jira.plugin.system.customfieldtypes:float"]');
-   
-  //  var auction_id = document.getElementsByID('customfield_15410-val').innerHTML;
-  
-  //  var auction = Number.parseInt(auction_id);
 
-   var supportTabOld,supportTabNew;
-   
-   if(findElement !== null )
-    {
-      console.log("Я нашел Support Tab");
-      findElement[1];
-     // innerText
-     //activating tab #customfield-panel-3
+  //supportTabNew = clicks;
 
-     supportTabOld = document.getElementById("customfield_15410-val").textContent;
-     supportTabOld = parseInt(supportTabOld);
-     console.log("Слышь, тут support tab =", supportTabOld);
-     
-     clicks = supportTabOld;
 
-     let section = document.querySelector('section');
-     section.innerHTML = '';
-     section.innerHTML += `<button class="btn" onclick="location.reload()" type="button">Обновить страницу</button>`;
+}
 
-    }
-
-    
   
 
 
@@ -69,9 +64,4 @@ function onClick() {
 
 
 
-//<div>
-// <button type="button" onClick="onClick()">Click me</button>
-// <p>Clicks: <a id="clicks">0</a></p>
-// </div>
-  
 
