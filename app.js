dbf8bd1a7997a1d17d34d63755a3780144934470
Your Name <you@example.com>
2021-03-02 23:13:33 +0300

    Changed logic in window, draw my icon only if it find Support Tab, added fetch to post my new support tab

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var clicks = 0;
+var jiraSupportTab;
 
 function include(url) {
     var script = document.createElement('script');
@@ -6,14 +7,20 @@ function include(url) {
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
-function onClick() {
+function plusClick() {
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks
   };
+
+// function devJira() {
+//     document.getElementById("devjira").innerHTML = jiraSupportTab
+//     console.log("Ты написал = ", jiraSupportTab)
+// };
+
 	
 
 var win_html3 = `<div style="cursor: -webkit-grab; display: flex; ">
-<button id="btn" type="button" onClick="onClick()"> Нажми на меня </button>
+<button id="btn" type="button" onclick="plusClick()"> Нажми на меня </button>
   <p>Количество обращений в Support tab: <a id="clicks">0</a></p> 
           </div>`;
 
@@ -21,53 +28,21 @@ var win_html3 = `<div style="cursor: -webkit-grab; display: flex; ">
 
 
 var url = window.location.href;
-
+let findComment = document.querySelector('[class = "chat-comment"]');
 
 
 if( url.includes('jira.skyeng.tech')){
-  let wint3 = document.createElement('div');
-  document.body.append(wint3);
-  wint3.style = 'min-height: 20px; max-height: 750px; min-width: 35px; max-width: 370px; background: wheat; top:405px ; left:562px;   font-size: 14px; border: 1px solid rgb(56, 56, 56); color: white;position: absolute; background: black;';
-  wint3.innerHTML = win_html3;
+  
 }
 else{
-  
-                
-  if(url.includes('skyeng.autofaq.ai')){
-    var win_html4 = `<div style="display: flex;  ">
-                  <span style="cursor: -webkit-grab;">
-                    <input id='devjira' style="margin:5px;placeholder="Вставь таску с jira сюда"> </input>
-                  </span>
-                </div>`;
+    if(findComment != null){
+      console.log("Я нашел твой комментарий");
 
-
-    let wint4 = document.createElement('div');
-    document.body.append(wint4);
-    wint4.style = 'min-height: 20px; max-height: 750px; min-width: 35px; max-width: 370px; back ground: wheat; top: 10px; left:500px; font-size: 14px; border: 1px solid rgb(56, 56, 56); color: white;position: absolute; z-index: 20; width: 10%; background: black;';
-    wint4.innerHTML = win_html4;  
-  }
-  
-}
-
-
-
-
-
-
-document.getElementById('btn').onclick = function (){
-  clicks += 1;
-  document.getElementById("clicks").textContent = clicks;
-
-  function myFunc () {
-    supportTabNew = clicks;
-    supportTabOld = supportTabNew;
-
-
-  }
-}
-
-
-
+      // supportTabOld = document.getElementById("customfield_15410-val").textContent;
+      // supportTabOld = parseInt(supportTabOld);
+      // console.log("Слышь, тут support tab =", supportTabOld);
+    };
+  };
 
 let findElement = document.querySelector('[data-fieldtypecompletekey="com.atlassian.jira.plugin.system.customfieldtypes:float"]');
 
@@ -75,7 +50,13 @@ var supportTabOld,supportTabNew;
 
 if(findElement !== null )
 {
-  console.log("Я нашел Support Tab");
+  // console.log("Я нашел Support Tab");
+  let wint3 = document.createElement('div');
+  document.body.append(wint3);
+  wint3.style = 'min-height: 20px; max-height: 750px; min-width: 35px; max-width: 370px; background: wheat; top:405px ; left:562px;   font-size: 14px; border: 1px solid rgb(56, 56, 56); color: white;position: absolute; background: black;';
+  wint3.innerHTML = win_html3; 
+
+
   findElement[1];
   supportTabOld = document.getElementById("customfield_15410-val").textContent;
   supportTabOld = parseInt(supportTabOld);
@@ -84,17 +65,37 @@ if(findElement !== null )
   clicks = supportTabOld;
   document.getElementById("clicks").textContent = clicks;
 
+  document.getElementById('btn').onclick = function (){
+    plusClick();
+    supportTabNew = clicks;
+    supportTabOld = supportTabNew;
+    var token = document.cookie.match(/atlassian.xsrf.token=(.*?);/)[1];
 
-  //supportTabNew = clicks;
+    let findissueID = document.getElementById("key-val");
 
+
+    if(findissueID!=null){
+      var issueID;
+      issueID = findissueID.getAttribute('rel');
+      };
+    
+    fetch("https://jira.skyeng.tech/secure/AjaxIssueAction.jspa?decorator=none", {
+      "headers": {
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                  },
+      "body": `customfield_15410=${supportTabOld}&issueId=${issueID}&atl_token=${token}`, //https://jira.skyeng.tech/browse/VIM-11161/
+      "method": "POST",
+      "credentials": "include"
+    })
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  };
 
 }
 
   
 
-
-
-
-
-
-
+// Чтобы залить на jira через AF нужно это делать через fetch 
+// отрисовать html страницу 
+// Потом match и найти совпадение по reports..
